d749140e7dca87cb625d45aaac02a44097bb9801
scorpi-zx <76877967+scorpi-zx@users.noreply.github.com>
2023-12-06 21:09:11 +0300

    Update color_handler_night_light.h

--- components/xiaomi_bslamp2/light/color_handler_night_light.h
+++ components/xiaomi_bslamp2/light/color_handler_night_light.h
@@ -51,7 +51,7 @@ class ColorHandlerNightLight : public ColorHandler {
       red = esphome::lerp(v.get_red(), 0.9997f, 0.9680f);
       green = esphome::lerp(v.get_green(), 0.9997f, 0.9600f);
       auto blue_scale = (v.get_red() + v.get_green()) / 2.0f;
-      auto blue_max = esphome::lerp(blue_scale, 0.9640f, 0.9520f);
+      auto blue_max = esphome::lerp(blue_scale, 0.9580f, 0.9500f);
       blue = esphome::lerp(v.get_blue(), 0.9997f, blue_max);
       white = 0.0f;
     }
