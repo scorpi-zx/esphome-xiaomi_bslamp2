bf822ae6cc587eb13d0281f85cc12f21c160f9f2
scorpi-zx <76877967+scorpi-zx@users.noreply.github.com>
2023-12-06 20:57:53 +0300

    Update color_handler_night_light.h

--- components/xiaomi_bslamp2/light/color_handler_night_light.h
+++ components/xiaomi_bslamp2/light/color_handler_night_light.h
@@ -39,9 +39,9 @@ class ColorHandlerNightLight : public ColorHandler {
     if (v.get_color_mode() == light::ColorMode::COLOR_TEMPERATURE) {
       red = 0.968f;
     // green = 0.968f;
-      green = 0.964f;
+      green = 0.960f;
     // blue = 0.972f;
-      blue = 0.968f;
+      blue = 0.960f;
       white = 0.0f;
     }
     // In RGB mode, the selected color is used to give the night light a
